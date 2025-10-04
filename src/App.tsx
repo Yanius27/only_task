970b8ef7c31a00b778627f9f15bafd4dae7b2dab
Yan <YaMSumarokov@1cbit.ru>
2025-10-04 22:17:37 +0700

    fix: Добавляет дополнительные тестовые данные 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import PeriodContainer from './components/PeriodContainer/PeriodContainer';
 import { PeriodProvider } from './context/PeriodContext';
 
 import { data as first_period } from '../lib/data';
+import { data_addition as second_period } from '../lib/data_addition';
 import FirstOrnament from './assets/icons/ornament-1.svg';
 import SecondOrnament from './assets/icons/ornament-2.svg';
 import CircleNavigation from './components/CircleNavigation/CircleNavigation';
 
+// Компоненты обёрнутые в PeriodProvider являются независимыми и могут получать данные в качестве пропсов
 export default function App() {
   return (
     <main>
       <div className="cont">
-        <PeriodProvider data={first_period}>
+        <PeriodProvider data={second_period}>
           <div className="cont__ornament">
             <FirstOrnament className="cont__ornament-first" />
             <SecondOrnament className="cont__ornament-second" />
